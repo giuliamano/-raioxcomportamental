dd7835547645fc2089ace737a737c73629a7c942
giuliamano <giuliapipolomano@gmail.com>
2025-07-19 23:04:38 -0300

    Update estilo.py

--- config/estilo.py
+++ config/estilo.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import datetime
 import gspread
-from config.estilo import aplicar_estilo
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
